d35641329a3bf8a5fbb69c55044738d27eadd70f
siyahgezegen <92273328+siyahgezegen@users.noreply.github.com>
2023-10-19 16:13:35 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,31 +1,12 @@
-package VeriYapilari;
+package VeriYapilari.DataStructure.Src;
 
-import VeriYapilari.LinkedList.LinkedList;
-import VeriYapilari.Model.Student;
+import VeriYapilari.DataStructure.Src.LinkedList.LinkedList;
 
 public class Main {
     public static void main(String[] args) {
-        int sayi = 2;
         LinkedList d1 = new LinkedList();
-        d1.AddElementInHead(new Student(5));
-        d1.AddElementInHead(new Student(12));
+        d1.AddElementInHead(null);
 
-        d1.AddElementInTail(new Student(25));
-        d1.AddElementInTail(new Student(27));
-        d1.AddElementInTail(new Student(29));
-        d1.AddElementInTail(new Student(32));
-        d1.AddElementInTail(new Student(35));
-        
-        d1.GetList();
-        
-        System.out.println("______________________");
-        
-        d1.DeleteElement(new Student(27));
-        
-        d1.GetList();
-        System.out.println("______________________");
-        d1.DeleteElementInHead();
-        d1.GetList();
     }
 
 }
