5ca8b23813b555eee6836892c34834e526c5f665
siyahgezegen <92273328+siyahgezegen@users.noreply.github.com>
2023-10-19 16:21:20 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,10 @@
 package VeriYapilari.DataStructure.Src;
 
-import VeriYapilari.DataStructure.Src.LinkedList.LinkedList;
+import VeriYapilari.DataStructure.Src.LinkedList.LinkedList;;
 
 public class Main {
     public static void main(String[] args) {
         LinkedList d1 = new LinkedList();
-        d1.AddElementInHead(null);
-        
-
     }
 
 }
