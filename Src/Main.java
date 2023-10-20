0a563819cb444c08f85a1045ff1220a9e901efad
siyahgezegen <92273328+siyahgezegen@users.noreply.github.com>
2023-10-20 16:15:33 +0300

    Ornek LinkList Kullanımı
    Yapılan Link Listin örnek kullanımı main.java içerisinde gösterildi

--- Src/Main.java
+++ Src/Main.java
@@ -1,10 +1,15 @@
 package VeriYapilari.DataStructure.Src;
 
-import VeriYapilari.DataStructure.Src.LinkedList.LinkedList;;
+import VeriYapilari.DataStructure.Src.LinkedList.LinkedList;
+import VeriYapilari.DataStructure.Src.Model.Student;;
 
 public class Main {
     public static void main(String[] args) {
-        LinkedList d1 = new LinkedList();
+        // Örnek LinkList Kullanımı
+        LinkedList examLinkedList = new LinkedList();
+        examLinkedList.AddElementInHead(new Student(25));
+        int a = examLinkedList.GetElementInList(0);
+        System.out.println(a);
     }
 
 }
